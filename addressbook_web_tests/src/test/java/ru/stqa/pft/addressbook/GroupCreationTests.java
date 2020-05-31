4a6cf80ead2a774e9270a93d0df8a223ccd64a24
AnnaMarcos <58533836+AnnaMarcos@users.noreply.github.com>
2020-05-30 20:29:02 -0700

    Made changes in @BeforeMethod

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -5,52 +5,51 @@ import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupCreationTest {
-  ///private WebDriver driver; rename variable 'driver'
-     private WebDriver wd;
-  // we do not need this variables anymore
- // private String baseUrl;
-  //private boolean acceptNextAlert = true;
-  //private StringBuffer verificationErrors = new StringBuffer();
+public class GroupCreationTests {
+  private WebDriver wd;
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
+
+  @BeforeMethod
+  public void setUp() {
     wd = new FirefoxDriver();
-    //baseUrl = "https://www.google.com/"; do not need anymore
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.id("LoginForm")).submit();
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("Test1");
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("Test2");
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("Test3");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.linkText("Logout")).click();
   }
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-    // do not need this verifications anymore
-    //String verificationErrorString = verificationErrors.toString();
-    //if (!"".equals(verificationErrorString)) {
-      //fail(verificationErrorString);
-   // }
+  @Test
+  public void testGroupCreation() {
+
+    wd.findElement(By.linkText("groups")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | link=groups | ]]
+    wd.findElement(By.name("new")).click();
+    wd.findElement(By.xpath("//div[@id='content']/form/label")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | //div[@id='content']/form/label | ]]
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=group_name | ]]
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys("test1");
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys("test2");
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys("test3");
+    wd.findElement(By.name("submit")).click();
+    wd.findElement(By.linkText("groups")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | link=groups | ]]
+    wd.findElement(By.linkText("Logout")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | link=Logout | ]]
   }
+
+  @AfterMethod
+  public void tearDown()  {
+    wd.quit();
+
+       }
 
   private boolean isElementPresent(By by) {
     try {
@@ -69,19 +68,5 @@ public class GroupCreationTest {
       return false;
     }
   }
-  // do not need this method anymore
-  //private String closeAlertAndGetItsText() {
-    //try {
-      //Alert alert = driver.switchTo().alert();
-      //String alertText = alert.getText();
-      //if (acceptNextAlert) {
-        //alert.accept();
-     // } else {
-       // alert.dismiss();
-      //}
-      //return alertText;
-    //} finally {
-     // acceptNextAlert = true;
-    }
-  //}
-//}
+
+  }
