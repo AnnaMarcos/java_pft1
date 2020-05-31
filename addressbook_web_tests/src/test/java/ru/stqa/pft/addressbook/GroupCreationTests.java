02c07823a085aa3b6d9ae4b3ce8e4d52518edf88
AnnaMarcos <58533836+AnnaMarcos@users.noreply.github.com>
2020-05-30 21:06:13 -0700

    Refactoring /GroupData creation

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -14,35 +14,61 @@ public class GroupCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    Login("admin", "secret");
+  }
+
+  private void Login(String username, String password) {
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.id("LoginForm")).submit();
   }
 
   @Test
   public void testGroupCreation() {
 
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test1", "test2", "test3"));
+    submitGroupCreation();
+    returnToGroupPage();
+    logOut();
+
+  }
+
+  private void logOut() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void returnToGroupPage() {
     wd.findElement(By.linkText("groups")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | link=groups | ]]
-    wd.findElement(By.name("new")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.xpath("//div[@id='content']/form/label")).click();
     // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | //div[@id='content']/form/label | ]]
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).click();
     // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=group_name | ]]
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("test1");
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("test2");
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("test3");
-    wd.findElement(By.name("submit")).click();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
     wd.findElement(By.linkText("groups")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | link=groups | ]]
-    wd.findElement(By.linkText("Logout")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | link=Logout | ]]
   }
 
   @AfterMethod
