c519d69e7231ea8c6ff55859658243d347d6553f
AnnaMarcos <58533836+AnnaMarcos@users.noreply.github.com>
2020-05-31 13:17:44 -0700

    Create Contact Data

---
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,67 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String lastName;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String cellPhone;
+  private final String email;
+  private final String dateOfBirth;
+  private final String monthOfBirth;
+  private final String yearOfBirth;
+
+  public ContactData(String firstName, String lastName, String title, String company, String address, String cellPhone, String email, String dateOfBirth, String monthOfBirth, String yearOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.cellPhone = cellPhone;
+    this.email = email;
+    this.dateOfBirth = dateOfBirth;
+    this.monthOfBirth = monthOfBirth;
+    this.yearOfBirth = yearOfBirth;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getCellPhone() {
+    return cellPhone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public String getMonthOfBirth() {
+    return monthOfBirth;
+  }
+
+  public String getYearOfBirth() {
+    return yearOfBirth;
+  }
+}
