8483f9ff2736b35faf468c5b84cfa63b3f299cdc
AnnaMarcos <58533836+AnnaMarcos@users.noreply.github.com>
2020-07-10 21:50:30 -0700

    3.10. Verification and execution of preconditions GroupTest

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/test/GroupDeletionTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/test/GroupDeletionTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.test;
 
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTest extends TestBase {
 
@@ -8,7 +9,10 @@ public class GroupDeletionTest extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
-
+    app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
+    }
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
