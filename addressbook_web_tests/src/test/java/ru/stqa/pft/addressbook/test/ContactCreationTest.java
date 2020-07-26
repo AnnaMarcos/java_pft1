0059b5ee8060ab92c895251741dc97d2034876f7
AnnaMarcos <58533836+AnnaMarcos@users.noreply.github.com>
2020-07-26 14:41:45 -0700

    4.9. Lambda Expressions (Anonymous Functions), Max Id estimation/Contacts

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/test/ContactCreationTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/test/ContactCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class ContactCreationTest extends TestBase {
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().returnToHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("Christian", "Silantyev",
+    ContactData contact = new ContactData("Fedorov", "Alex",
             "25803 Anderson Ln", "8184306300", "annasilantyeva@gmail.com","Test1");
     app.getContactHelper().createContact(contact,true);
     app.getNavigationHelper().returnToHomePage();
@@ -21,13 +22,8 @@ public class ContactCreationTest extends TestBase {
     Assert.assertEquals(after.size(),before.size() + 1);
     app.getSessionHelper().logout();
 
-    int max = 0;
-    for (ContactData c : after){
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+
+    contact.setId(after.stream().max((Comparator<ContactData>) (o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add (contact);
 
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
