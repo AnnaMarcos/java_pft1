7d6b2244093e8baf989d4ff724ecd3afab2a1316
AnnaMarcos <58533836+AnnaMarcos@users.noreply.github.com>
2020-08-04 23:13:12 -0700

    6.3. Command line parameters when starting the data generator/GroupDataGeneration

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTest.java
@@ -1,13 +1,30 @@
 package ru.stqa.pft.addressbook.test;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase{
+
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{ new GroupData().withName("test 1").withHeader( "header 1").withFooter( "footer 1")});
+    list.add(new Object[]{ new GroupData().withName("test 2").withHeader( "header 2").withFooter( "footer 2")});
+    list.add(new Object[]{ new GroupData().withName("test 3").withHeader( "header 3").withFooter( "footer 3")});
+
+    return list.iterator();
+
+
+  }
 
   @Test
   public void testGroupCreation() throws Exception {
@@ -23,6 +40,19 @@ public class GroupCreationTest extends TestBase{
             (group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
   }
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreationNew(GroupData group) throws Exception {
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+      app.group().create(group);
+      app.goTo().groupPage();
+      assertThat(app.group().count(), equalTo(before.size() + 1));
+      Groups after = app.group().all();
+      assertThat(after, equalTo(before.withAdded
+              (group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
+    }
+
 
   @Test
   public void testBadGroupCreation() throws Exception {
